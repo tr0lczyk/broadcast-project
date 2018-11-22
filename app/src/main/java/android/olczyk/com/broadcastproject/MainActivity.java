093a22d8a6458d9cbfd570cc4180be5254713bc8
Mateusz <30321069+tr0lczyk@users.noreply.github.com>
2018-11-22 13:05:42 +0100

    ordered broadcast implementation

--- app/src/main/java/android/olczyk/com/broadcastproject/MainActivity.java
+++ app/src/main/java/android/olczyk/com/broadcastproject/MainActivity.java
@@ -10,8 +10,18 @@ import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+    OrderedReceiver1 orderedReceiver1 = new OrderedReceiver1();
 
     @AfterViews
     public void aVoid(){
+        IntentFilter filter = new IntentFilter("android.olczyk.com.broadcastproject.EXAMPLE_ACTION");
+        filter.setPriority(1);
+        registerReceiver(orderedReceiver1,filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(orderedReceiver1);
     }
 }
---
+++ app/src/main/java/android/olczyk/com/broadcastproject/OrderedReceiver1.java
@@ -0,0 +1,35 @@
+package android.olczyk.com.broadcastproject;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class OrderedReceiver1 extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int resultCode = getResultCode();
+        String resultData = getResultData();
+        Bundle resultExtras = getResultExtras(true);
+        String stringExtra = resultExtras.getString("stringExtra");
+
+        resultCode++;
+        stringExtra += "->OR1";
+
+        String toastText = "OR1\n" + "resultCode: " +
+                resultCode + "\n" + "resultData: " +
+                resultData +"\n" + "stringExtra: " +
+                stringExtra;
+
+
+        Toast.makeText(context, toastText, Toast.LENGTH_LONG).show();
+        resultData = "OR1";
+        resultExtras.putString("stringExtra", stringExtra);
+
+        setResult(resultCode, resultData, resultExtras);
+    }
+}
